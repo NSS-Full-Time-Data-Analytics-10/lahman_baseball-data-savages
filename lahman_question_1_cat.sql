d0389c8763a50b729567338a5f308956de05bb6a
Cathy Hoang <thoang629@gmail.com>
2023-06-06 16:24:45 -0400

    lahman_q1_cat

---
+++ lahman_question_1_cat.sql
@@ -0,0 +1,8 @@
+---1. What range of years for baseball games played does the provided database cover?
+SELECT MIN(year), MAX(year) 
+FROM homegames;
+
+SELECT MIN(yearid), MAX(yearid) 
+FROM teams
+
+---***---1871-2016
