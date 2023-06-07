4e3011005cc66b759a4b31772d1d90f79e7c26c2
Cathy Hoang <thoang629@gmail.com>
2023-06-07 12:28:17 -0400

    lahman_q4_cat

--- lahman_q4_cat.sql
+++ lahman_q4_cat.sql
@@ -3,16 +3,22 @@
 ------those with position "SS", "1B", "2B", and "3B" as "Infield", 
 ------and those with position "P" or "C" as "Battery". 
 ---Determine the number of putouts made by each of these three groups in 2016.
+---ANSWER*-*-*-* 41424	"Battery"
+---------*-*-*-* 58934	"Infield"
+---------*-*-*-* 29560	"Outfield"
 
----???can we put multiple conditions in one single case statment
-
-SELECT Pos, 
-	COUNT(CASE WHEN Pos = 'OF' 
-		  THEN 'Outfield'END) AS outfield_count,
-	COUNT(CASE WHEN Pos = 'SS' OR Pos = '1B' 
-		  		 OR Pos = '2B' OR Pos = '3B' 
-		  THEN 'Infield'END) AS infield_count,
-	COUNT(CASE WHEN Pos = 'P' OR Pos = 'C' 
-		  THEN 'Battery'END) AS battery_count 
+SELECT SUM(po) AS putout_sum,
+	CASE WHEN Pos = 'OF' 
+		  THEN 'Outfield'
+		 WHEN Pos = 'SS' OR Pos = '1B' 
+		   OR Pos = '2B' OR Pos = '3B' 
+		  THEN 'Infield'
+		 WHEN Pos = 'P' OR Pos = 'C' 
+		  THEN 'Battery' END AS pos_type
 FROM fielding
-GROUP BY Pos
+WHERE yearid = 2016
+GROUP BY pos_type
+
+
+
+
